9d265ec3feca6c45d8edfe16ab04eef579d0951c
Chan Hen <officialchanhen@gmail.com>
2026-04-14 18:24:57 -0500

    First two pages done

--- src/app/(tabs)/about/page.tsx
+++ src/app/(tabs)/about/page.tsx
@@ -14,7 +14,10 @@ gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 export default function About() {
     const textContainer = useRef<HTMLDivElement>(null);
-    const techStackStyle = "tech-content opacity-0 w-full flex flex-col justify-start items-start bg-primary/60 border border-primary backdrop-blur-xs rounded-xl p-10 gap-5";
+    const techStackStyle = "group opacity-0 w-full flex flex-col justify-start items-start p-10 gap-5 bg-gradient-to-br from-white/5 to-white/[0.02] border border-white/10 text-white rounded-xl";
+    const techImg = "place-self-center md:place-self-start flex flex-col md:flex-row justify-center md:justify-start items-center gap-3 text-[1rem] md:text-[1.25rem] font-bold group-hover:scale-105 transition-transform ease-in-out duration-300";
+    const techTitleStyle = "group-hover:text-highlight transition-color ease-in-out duration-300";
+    const techSubListStyle = "list-disc marker:text-highlight pl-5 space-y-2 text-[0.75rem] md:text-[1rem] text-white/80 group-hover:text-white transition-color group-hover:scale-105 transition-transform ease-in-out duration-300";
 
     const navigationMode = useNavigationMode();
     const delayTime = navigationMode === "soft" ? 0.4 : 0.6;
@@ -57,7 +60,7 @@ export default function About() {
                 },
                 0
             )
-        
+
         const panels = gsap.utils.toArray<HTMLDivElement>(".panel");
         const panelSize = panels[0]?.offsetHeight ?? 0;
         const endPoint = panelSize;
@@ -71,38 +74,61 @@ export default function About() {
                 pinSpacing: true,
             });
         });
-        
-        gsap.fromTo(".tech-content", 
-            {
-                y: 30,
-                opacity: 0,
-            },
-            {
-                opacity: 1, 
-                y: 0, 
-                ease: "power2.out",
-                stagger: {
-                    amount: 0.1,
-                    from: "start",
-                    ease: "power2.inOut",
+
+        const fadeInElements = gsap.utils.toArray<Element>(".fade-in")
+        fadeInElements.forEach((fadeInElement) => {
+            gsap.fromTo(fadeInElement, 
+                {
+                    y: 30,
+                    opacity: 0,
                 },
-                scrollTrigger: {
-                    trigger: ".tech-header",
-                    start: "top top",
-                    end: () => `+=${endPoint}`,
-                    toggleActions: "play none none reverse",
-                    scrub: 1,
-                    markers: true
+                {
+                    opacity: 1, 
+                    y: 0, 
+                    duration: 1,
+                    ease: "power2.out",
+                    scrollTrigger: {
+                        trigger: fadeInElement,
+                        start: "top 60%",
+                        end: () => `bottom 50%`,
+                        markers: true
+                    }
                 }
-            }
-        )
+            );
+        });
+
+        const fadeInList = gsap.utils.toArray<Element>(".fade-in-list")
+        fadeInList.forEach((fadeInList) => {
+            gsap.fromTo(fadeInList.children, 
+                {
+                    y: 30,
+                    opacity: 0,
+                },
+                {
+                    opacity: 1, 
+                    y: 0, 
+                    duration: 1,
+                    ease: "power2.out",
+                    stagger: {
+                        each: 0.3,
+                        from: "start",
+                    },
+                    scrollTrigger: {
+                        trigger: fadeInList,
+                        start: "top 60%",
+                        end: () => `bottom 50%`,
+                        markers: true
+                    }
+                }
+            );
+        });
     })
 
     return(
         <div className="w-full h-full flex flex-col justify-start md:justify-center items-center">
             <StarBackground/>
             {/* Header page */}
-            <div className="min-w-screen flex flex-col justify-start items-center p-10 gap-10">
+            <div className="min-w-screen min-h-screen flex flex-col justify-center items-center p-10 gap-10 bg-primary">
                 {/* Header Info */}
                 <div ref={textContainer} className="text-container relative flex flex-col justify-center items-center text-center gap-2">
                     {/* Rocket */}
@@ -126,7 +152,7 @@ export default function About() {
                     
                     {/* Resume */}
                     <div className="intro-text">
-                        <div className="flex flex-row justify-center items-center gap-2 px-5 py-3 rounded-md cursor-pointer bg-tertiary border-none shadow-[0_0_25px] shadow-tertiary backdrop-blur-xs hover:shadow-[0_0_5px,_0_0_25px,_0_0_50px,_0_0_100px] hover:shadow-tertiary hover:scale-105 text-nowrap transition-all ease-in-out duration-300">
+                        <div className="flex flex-row justify-center items-center gap-2 px-5 py-3 mt-5 rounded-md cursor-pointer bg-tertiary border-none shadow-[0_0_25px] shadow-tertiary backdrop-blur-xs hover:shadow-[0_0_5px,_0_0_25px,_0_0_50px,_0_0_100px] hover:shadow-tertiary hover:scale-105 text-nowrap transition-all ease-in-out duration-300">
                             <a href="/Official-Resume.pdf" download className="text-white/80 font-bold">Download Resume</a>
                             <Download className="w-[clamp(18px,2vw,20px)] h-[clamp(18px,2vw,20px)]"/>
                         </div>
@@ -135,25 +161,25 @@ export default function About() {
                 </div>
             </div>
             {/* Tech page */}
-            <div className="panel min-w-screen min-h-screen flex flex-col justify-center items-center p-10 gap-10">
+            <div className="min-w-screen min-h-screen flex flex-col justify-center items-center p-10 gap-10 bg-secondary">
                 {/* Header */}
-                <h2 className="tech-header tech-content opacity text-[2.5rem] md:text-[3.5rem]">
+                <h2 className="fade-in opacity-0 text-[2.5rem] md:text-[3.5rem]">
                     <span className="bg-gradient-to-t from-white via-highlight to-tertiary bg-clip-text text-transparent">
                         Tech Stack
                     </span>
                 </h2>
 
                 {/* Tech Stack */}
-                <div className="md:w-full max-w-[1080px] flex flex-col md:flex-row justify-center md:items-stretch gap-5">
+                <div className="fade-in-list md:w-full max-w-[1080px] flex flex-col md:flex-row justify-center md:items-stretch gap-5">
                     {/* Frontend */}
                     <div className={techStackStyle}>
-                        <div className="place-self-center md:place-self-start flex flex-col md:flex-row justify-center md:justify-start items-center gap-3 text-[1rem] md:text-[1.25rem] font-bold">
+                        <div className={techImg}>
                             <div className="relative w-[clamp(24px,2vw,30px)] h-[clamp(24px,2vw,30px)] flex flex-row justify-center items-center p-5 bg-linear-to-br from-tertiary to-highlight text-white rounded-md">
                                 <AppWindow className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[clamp(18px,2vw,20px)] h-[clamp(18px,2vw,20px)]"/>
                             </div>
-                            <span>Frontend</span>
+                            <span className={techTitleStyle}>Frontend</span>
                         </div>
-                        <ul className="list-disc marker:text-highlight pl-5 space-y-2 text-[0.75rem] md:text-[1rem] text-white/80">
+                        <ul className={techSubListStyle}>
                             <li>Typescript</li>
                             <li>React</li>
                             <li>Tailwind CSS</li>
@@ -164,13 +190,13 @@ export default function About() {
 
                     {/* Backend */}
                     <div className={techStackStyle}>
-                        <div className="place-self-center md:place-self-start flex flex-col md:flex-row justify-center md:justify-start items-center gap-3 text-[1rem] md:text-[1.25rem] font-bold">
+                        <div className={techImg}>
                             <div className="relative w-[clamp(24px,2vw,30px)] h-[clamp(24px,2vw,30px)] flex flex-row justify-center items-center p-5 bg-linear-to-br from-tertiary to-highlight text-white rounded-md">
                                 <Database className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[clamp(18px,2vw,20px)] h-[clamp(18px,2vw,20px)]"/>
                             </div>
-                            Backend
+                            <span className={techTitleStyle}>Backend</span>
                         </div>
-                        <ul className="list-disc marker:text-highlight pl-5 space-y-2 text-[0.75rem] md:text-[1rem] text-white/80">
+                        <ul className={techSubListStyle}>
                             <li>Python</li>
                             <li>Java</li>
                             <li>C</li>
@@ -181,13 +207,13 @@ export default function About() {
 
                     {/* Tools */}
                     <div className={techStackStyle}>
-                        <div className="place-self-center md:place-self-start flex flex-col md:flex-row justify-center md:justify-start items-center gap-3 text-[1rem] md:text-[1.25rem] font-bold">
+                        <div className={techImg}>
                             <div className="relative w-[clamp(24px,2vw,30px)] h-[clamp(24px,2vw,30px)] flex flex-row justify-center items-center p-5 bg-linear-to-br from-tertiary to-highlight text-white rounded-md">
                                 <Wrench className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[clamp(18px,2vw,20px)] h-[clamp(18px,2vw,20px)]"/>
                             </div>
-                            Tools
+                             <span className={techTitleStyle}>Tools</span>
                         </div>
-                        <ul className="list-disc marker:text-highlight pl-5 space-y-2 text-[0.75rem] md:text-[1rem] text-white/80">
+                        <ul className={techSubListStyle}>
                             <li>Figma</li>
                             <li>Git</li>
                             <li>Github</li>
@@ -199,9 +225,9 @@ export default function About() {
             </div>
 
             {/* Values Page */}
-            <div className="panel panel__values min-w-screen min-h-screen flex flex-col justify-center items-center p-10 gap-10 border border-white">
+            <div className="min-w-screen min-h-screen flex flex-col justify-center items-center p-10 gap-10 bg-primary">
                 {/* Header */}
-                <h2 className="intro-text text-[2.5rem] md:text-[3.5rem]">
+                <h2 className="fade-in intro-text text-[2.5rem] md:text-[3.5rem]">
                     {"What I "}
                     <span className="bg-gradient-to-t from-white via-highlight to-tertiary bg-clip-text text-transparent">
                         Value
@@ -209,7 +235,7 @@ export default function About() {
                 </h2>
 
                 {/* Values */}
-                <div className="md:w-full max-w-[1080px] flex flex-col md:flex-row justify-center md:items-stretch gap-5">
+                <div className="fade-in-list md:w-full max-w-[1080px] flex flex-col md:flex-row justify-center md:items-stretch gap-5">
 
                     {/* Code */}
                     <div className="group w-full flex flex-col justify-start items-center gap-3 text-[1rem] md:text-[1.5rem]">
@@ -233,7 +259,7 @@ export default function About() {
 
                     {/* Collaboration */}
                     <div className="group w-full flex flex-col justify-start items-center gap-3 text-[1rem] md:text-[1.5rem]">
-                        <div className="relative w-[clamp(60px,8vw,84px)] h-[clamp(60px,8vw,84px)] flex flex-row justify-center items-center p-5 bg-gradient-to-br from-white/5 to-white/[0.02] border border-white/10 text-purple-300  rounded-xl group-hover:scale-105 transition-transform ease-in-out duration-300">
+                        <div className="relative w-[clamp(60px,8vw,84px)] h-[clamp(60px,8vw,84px)] flex flex-row justify-center items-center p-5 bg-gradient-to-br from-white/5 to-white/[0.02] border border-white/10 text-purple-300 rounded-xl group-hover:scale-105 transition-transform ease-in-out duration-300">
                             <div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[clamp(72px,10vw,96px)] h-[clamp(72px,10vw,96px)] flex flex-row justify-center items-center p-5 rounded-2xl bg-gradient-to-r from-pink-400 to-purple-500 opacity-0 group-hover:opacity-30 blur-xl transition-opacity duration-300"/>
                             <Users className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[clamp(36px,5vw,48px)] h-[clamp(36px,5vw,48px)]"/>
                         </div>
@@ -244,12 +270,12 @@ export default function About() {
             </div>
 
             {/* Music Page */}
-            <div className="panel min-w-screen min-h-screen flex flex-col justify-center items-center p-10 gap-10 border border-white">
+            <div className="min-w-screen min-h-screen flex flex-col justify-center items-center p-10 gap-10 bg-secondary">
                 <SpotifyTopTracks/>
             </div>
 
             {/* Games Page */}
-            <div className="panel min-w-screen min-h-screen flex flex-col justify-center items-center p-10 gap-10 border border-white">
+            <div className="min-w-screen min-h-screen flex flex-col justify-center items-center p-10 gap-10 bg-primary">
                 <TopGames/>
             </div>
         </div>
